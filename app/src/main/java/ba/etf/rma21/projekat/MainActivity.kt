13f09f3cf760406f0b8edfb96386d58b314a99ee
osaric1 <osaric1@etf.unsa.ba>
2021-03-31 19:50:25 +0200

    pocetna verzija element_liste.xml

--- app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
@@ -2,13 +2,18 @@ package ba.etf.rma21.projekat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listaKvizova: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
 
