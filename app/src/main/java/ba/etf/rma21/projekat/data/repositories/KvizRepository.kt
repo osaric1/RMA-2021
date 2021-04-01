a0e5516a5e4d8a3369e6670686045d74cf439143
osaric1 <osaric1@etf.unsa.ba>
2021-04-01 16:46:19 +0200

    sitne popravke UI

--- app/src/main/java/ba/etf/rma21/projekat/data/repositories/KvizRepository.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/repositories/KvizRepository.kt
@@ -19,9 +19,8 @@ class KvizRepository {
                     Kviz("IM Kviz", "IM", Date(2021, 4,1),Date(2021,4,1), Date(2021,4,1), 30,"IM_Grupa_1", 10f),
                     Kviz("IM Kviz", "IM", Date(2021, 5,6),Date(2021,5,6), Date(2021,5,6), 30,"IM_Grupa_2", 10f),
                     Kviz("OE Kviz", "OE", Date(2021, 4,7),Date(2021,4,7), Date(2021,4,7), 60,"OE_Grupa_1", 40f),
-                    Kviz("OE Kviz", "OE", Date(2021, 4,7),Date(2021,4,7), Date(2021,4,7), 60,"OE_Grupa_2", 40f),
-                    Kviz("OE Kviz", "OE", Date(2021, 4,7),Date(2021,4,7), Date(2021,4,7), 60,"OE_Grupa_2", 40f))
-        }
+                    Kviz("OE Kviz", "OE", Date(2021, 4,7),Date(2021,4,7), Date(2021,4,7), 60,"OE_Grupa_2", 40f)
+            )        }
 
         fun getMyKvizes(): List<Kviz> {
             return kvizovi.filter { kviz -> kviz.nazivGrupe == "DM_Grupa_1" && kviz.nazivPredmeta == "DM" }.toList()
