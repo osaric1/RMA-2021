b2a3213c7d50146026f52cc8c01ab12cdebbfb28
osaric1 <osaric1@etf.unsa.ba>
2021-04-01 10:58:44 +0200

    floating button

--- app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma21/projekat/MainActivity.kt
@@ -1,17 +1,29 @@
 package ba.etf.rma21.projekat
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listaKvizova: RecyclerView
+    private lateinit var  spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        spinner = findViewById(R.id.filterKvizova)
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.spinner_choices,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
 
 
     }
